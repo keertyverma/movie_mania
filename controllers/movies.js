cc4aaa420539b919f0cb3e19666bc3ff65be6c8c
Keerty Verma <keertyverma1303@gmail.com>
2023-02-08 14:58:28 +0530

    added get and post request for /movies route and connected with mongodb

---
+++ controllers/movies.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const { StatusCodes } = require("http-status-codes");
+
+const logger = require("../utils/logger");
+const { Movie, Genre } = require("../models/movie");
+
+const movieRoute = "/movies";
+
+const getAllMovies = async (req, res) => {
+  logger.debug(`GET Request on Route -> ${movieRoute}/`);
+  const movies = await Movie.find();
+  res.send(movies);
+};
+
+const createGenre = async (genres) => {
+  const insertGenres = genres.map((genre) => {
+    return { name: genre };
+  });
+
+  const genreResult = await Genre.create(insertGenres);
+  logger.debug(`Genres are added.`);
+  return genreResult;
+};
+
+const createMovie = async (req, res) => {
+  logger.debug(`POST Request on Route -> ${movieRoute}/`);
+
+  const movie = {
+    name: req.body.name,
+  };
+
+  const genres = req.body.genres;
+  if (genres) {
+    const movieGenres = await createGenre(genres);
+    movie.genres = movieGenres;
+  }
+
+  if (req.body.release_date) {
+    movie.release_date = req.body.release_date;
+  }
+  if (req.body.poster_url) {
+    movie.poster_url = req.body.poster_url;
+  }
+
+  logger.debug("creating new movie document");
+  try {
+    const movieResult = await Movie.create(movie);
+    logger.debug(`new movie is added with id = ${movieResult.id}`);
+
+    res.status(StatusCodes.CREATED).json(movieResult);
+  } catch (exceptions) {
+    let validationErrors = [];
+    for (field in exceptions.errors) {
+      validationErrors.push(exceptions.errors[field].message);
+    }
+    res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ name: "ValidationError", message: validationErrors.join(", ") });
+  }
+};
+
+module.exports = {
+  getAllMovies,
+  createMovie,
+};
