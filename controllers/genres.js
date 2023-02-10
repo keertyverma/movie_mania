3c66f3c1f93b2eaf78e1764021ecf0a60367149e
Keerty Verma <keerty@keertyverma1303@gmail.com>
2023-02-10 12:49:35 +0530

    post /genres route is added

--- controllers/genres.js
+++ controllers/genres.js
@@ -1,10 +1,27 @@
+const { StatusCodes } = require("http-status-codes");
+const logger = require("../utils/logger");
+
 const { Genre } = require("../models/genre");
 
+const genreRoute = "/genres";
+
 const getAllGenres = async (req, res) => {
+  logger.debug(`GET Request on Route -> ${genreRoute}/`);
   const genres = await Genre.find().select("name").sort("name");
-  res.send(genres);
+  res.status(StatusCodes.OK).json(genres);
+};
+
+const createGenre = async (req, res) => {
+  logger.debug(`POST Request on Route -> ${genreRoute}/`);
+
+  logger.debug("creating new genre document");
+  const genre = await Genre.create({ name: req.body.name });
+
+  logger.debug(`new genre is added with id = ${genre.id}`);
+  res.status(StatusCodes.CREATED).json(genre);
 };
 
 module.exports = {
   getAllGenres,
+  createGenre,
 };
